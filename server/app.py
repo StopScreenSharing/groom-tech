3c0e290cfea34a18422320f93877fd5ce5cb6219
aidadieudonne <aidaharing10@gmail.com>
2026-02-05 11:31:23 -0700

    create Signup, Login, Logout, and CheckSession

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, session
 from flask_restful import Resource
 
 # Local imports
@@ -18,11 +18,13 @@ from schemas import (
     AppointmentSchema,
     AppointmentDetailSchema
 )
+from datetime import date
+
 
 
 # Views go here!
 
-class Groomers(Resource):
+class Signup(Resource):
     def post(self):
         data = request.get_json()
 
@@ -36,9 +38,43 @@ class Groomers(Resource):
         db.session.add(groomer)
         db.session.commit()
 
+        session["groomer_id"] = groomer.id
+
         return GroomerSchema().dump(groomer), 201
+    
 
 
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        groomer = Groomer.query.filter_by(employee_number=data["employee_number"]).first()
+        
+        if not groomer or not groomer.authenticate(data["password"]):
+            return {"error": "Invalid credentials"}, 401
+        
+        session["groomer_id"] = groomer.id
+        return GroomerSchema().dump(groomer), 200
+
+class CheckSession(Resource):
+    def get(self):
+       groomer_id = session.get("groomer_id")
+
+       if not groomer_id:
+           return {"message": "Not Authorized"}, 401
+
+       groomer = Groomer.query.get(groomer_id)
+       return GroomerDashboardSchema().dump(groomer), 200        
+
+class Logout(Resource):
+    def delete(self):
+        session.pop("groomer_id", None)
+        return {}, 204
+
+
+api.add_resource(Signup, "/signup")
+api.add_resource(Login, "/login")
+api.add_resource(CheckSession, "/check_session")
+api.add_resource(Logout, "/logout")
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
